c081f995ecbeb670d9bd76b6c41b6662adc62db5
KateZ <zakusova30@gmail.com>
2020-12-14 20:03:06 +0300

    Create skelet faila

---
+++ git2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int(vib);
+
+int main() {
+	//Ввод переменных
+	switch (vib)
+	{
+	case '1': //Сложение
+	case '2': //Вычитание
+	case '3': //Деление
+	case '4': //Умножение
+	}
+}
